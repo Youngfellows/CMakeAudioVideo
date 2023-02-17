b6f1857a82d8344a7ca5819415951eb6598dc8cb
Youngfellow <Youngfellow@163.com>
2023-02-17 09:57:12 +0800

    xxxxx

--- CMakeBase/16-CMake/make1.sh
+++ CMakeBase/16-CMake/make1.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 rm -r build
 mkdir build && cd build
-cmake .. -DDEBUG=NO -L
+cmake .. -DDEBUG=NO -DRELEASE=Sample20230217.apk -L
 make
 cd ..
 # ./build/Sample 2 3
--- CMakeBase/16-CMake/make2.sh
+++ CMakeBase/16-CMake/make2.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 rm -r build
 mkdir build && cd build
-cmake .. -DDEBUG=YES -DRELEASE=NO -L
+cmake .. -DDEBUG=YES -L
 make
 cd ..
 # ./build/Sample 2 3
