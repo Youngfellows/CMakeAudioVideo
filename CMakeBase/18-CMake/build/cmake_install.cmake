dbb6254ada69fde692b64a5560923a5f0be24256
Youngfellow <Youngfellow@163.com>
2023-02-17 11:21:25 +0800

    xxxx

--- CMakeBase/18-CMake/build/cmake_install.cmake
+++ CMakeBase/18-CMake/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/d/TempCPPCode2023/NDK-CMake/CMakeAudioVideo/08-CMake
+# Install script for directory: /mnt/d/TempCPPCode2023/NDK-CMake/CMakeAudioVideo/CMakeBase/18-CMake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/d/TempCPPCode2023/NDK-CMake/CMakeAudioVideo/08-CMake/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/d/TempCPPCode2023/NDK-CMake/CMakeAudioVideo/CMakeBase/18-CMake/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
