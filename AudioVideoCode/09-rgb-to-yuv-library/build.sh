41493106f6ac9fe05f7a7eddc19790dc9b9c5df3
Youngfellow <Youngfellow@163.com>
2023-03-27 15:22:10 +0800

    xxxx

---
+++ AudioVideoCode/09-rgb-to-yuv-library/build.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+PROJ_ROOT=$PWD
+BUILD_ROOT=${PROJ_ROOT}
+echo "build root: ${BUILD_ROOT}"
+
+# 设置resource目录
+RESOURCE_DIR=${BUILD_ROOT}/resource
+if [ -e ${RESOURCE_DIR} ]; then
+    echo "rm -rf ${RESOURCE_DIR}"
+    rm -rf ${RESOURCE_DIR}
+fi
+mkdir -p ${RESOURCE_DIR}
+
+# 设置install目录
+INSTALL_DIR=${BUILD_ROOT}/install
+if [ -e ${INSTALL_DIR} ]; then
+    echo "rm -rf ${INSTALL_DIR}"
+    rm -rf ${INSTALL_DIR}
+fi
+mkdir -p ${INSTALL_DIR}
+
+# 设置build编译目录
+BUILD_DIR=${BUILD_ROOT}/build
+if [ -e ${BUILD_DIR} ]; then
+    echo "rm -rf ${BUILD_DIR}"
+    rm -rf ${BUILD_DIR}
+fi
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+# 编译
+cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX:PATH=${PROJ_ROOT}/install $PROJ_ROOT
+make -j4
+make install
+cd -
+
+# 运行
+WIDTH=711
+HEIGHT=711
+# ${BUILD_ROOT}/install/bin/08-rgb24-to-yuv420 ${WIDTH} ${HEIGHT}
+# ${BUILD_ROOT}/install/bin/08-yuv444-to-rgb24 ${WIDTH} ${HEIGHT}
+# ${BUILD_ROOT}/install/bin/08-yuv444-to-yuv420 ${WIDTH} ${HEIGHT}
+# ${BUILD_ROOT}/install/bin/08-yuv420-to-yuv444 ${WIDTH} ${HEIGHT}
---
+++ AudioVideoCode/3rd/descampsa/yuv2rgb/build.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PROJ_ROOT=$PWD
+BUILD_ROOT=${PROJ_ROOT}
+echo "build root: ${BUILD_ROOT}"
+
+# 设置resource目录
+RESOURCE_DIR=${BUILD_ROOT}/resource
+if [ -e ${RESOURCE_DIR} ]; then
+    echo "rm -rf ${RESOURCE_DIR}"
+    rm -rf ${RESOURCE_DIR}
+fi
+mkdir -p ${RESOURCE_DIR}
+
+# 设置install目录
+INSTALL_DIR=${BUILD_ROOT}/install
+if [ -e ${INSTALL_DIR} ]; then
+    echo "rm -rf ${INSTALL_DIR}"
+    rm -rf ${INSTALL_DIR}
+fi
+mkdir -p ${INSTALL_DIR}
+
+# 设置build编译目录
+BUILD_DIR=${BUILD_ROOT}/build
+if [ -e ${BUILD_DIR} ]; then
+    echo "rm -rf ${BUILD_DIR}"
+    rm -rf ${BUILD_DIR}
+fi
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+# 编译
+cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX:PATH=${PROJ_ROOT}/install $PROJ_ROOT
+make -j4
+make install
+cd -
+
+# 运行
+:<<!
+Usage : test yuv2rgb <yuv image file> <image width> <image height> <output template filename>
+Or    : test yuv2rgb_nv12 <yuv image file> <image width> <image height> <output template filename>
+Or    : test yuv2rgb_nv21 <yuv image file> <image width> <image height> <output template filename>
+Or    : test rgb2yuv <rgb24 binary ppm image file> <output template filename>
+Or    : test rgba2yuv <rgb24 binary ppm image file> <output template filename>
+!
+WIDTH=711
+HEIGHT=711
+${BUILD_ROOT}/install/bin/test_yuv_rgb rgb2yuv ${PROJ_ROOT}/data/test1.ppm ${RESOURCE_DIR}/test1_ppm_rgb24_to_yuv.yuv
