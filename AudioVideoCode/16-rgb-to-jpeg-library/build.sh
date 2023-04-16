415ac8f2236df1afe9f7212897f3f2d5ce6c5f79
Youngfellow <Youngfellow@gmail.com>
2023-04-16 18:09:06 +0800

    xxxx

--- AudioVideoCode/16-rgb-to-jpeg-library/build.sh
+++ AudioVideoCode/16-rgb-to-jpeg-library/build.sh
@@ -29,6 +29,16 @@ fi
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
+# 设置so动态库查找路径
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+echo "current dir: ${CURRENT_DIR}"
+PARENT_DIR=$(cd $(dirname $CURRENT_DIR); pwd)
+echo "parent dir: ${PARENT_DIR}"
+
+# export LD_LIBRARY_PATH=${PARENT_DIR}/install/lib/libturbojpeg.so
+export LD_LIBRARY_PATH=${PARENT_DIR}/3rd/libjpeg-turbo/install/lib/
+echo "library path:${LD_LIBRARY_PATH}"
+
 # 编译
 cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} $PROJ_ROOT
