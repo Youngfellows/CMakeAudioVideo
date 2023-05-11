5314cee02513e5a76ccb834735059121dc1cb868
Youngfellow <Youngfellow@163.com>
2023-05-11 18:13:56 +0800

    xxxx

---
+++ AudioVideoCode/21-gif-decoder-encoder/src/SampleGifDecoder.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(int arc, char *argv[])
+{
+    const char *tag = "SampleGifDecoder::";
+    printf("%s%s,Line %ld\n", tag, __FUNCTION__, __LINE__);
+    return 0;
+}
--- AudioVideoCode/21-gif-decoder-encoder/src/cpp/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main(int arc, char *argv[])
-{
-    printf("xxxxxxxx\n");
-    return 0;
-}
