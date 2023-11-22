642cd502bf9419a12267de18681c4e82a00bb234
Youngfellow <Youngfellow@163.com>
2023-11-22 15:35:50 +0800

    xxxx

--- AudioVideoCode/01-android-ndk-archabi-chatgpt/build.sh
+++ AudioVideoCode/01-android-ndk-archabi-chatgpt/build.sh
@@ -4,41 +4,47 @@ PROJ_ROOT=$PWD
 BUILD_ROOT=$PROJ_ROOT
 echo "build root: $BUILD_ROOT"
 
-# 先删除旧的安装文件
-if [ -e $BUILD_ROOT/dist ] ;then
-    echo "rm ${BUILD_ROOT}/dist"
-    rm -rf ${BUILD_ROOT}/dist
+
+# 设置Release目录
+RELEASE_DIR=${BUILD_ROOT}/Release
+if [ -e ${RELEASE_DIR} ]; then
+    echo "rm -rf ${RELEASE_DIR}"
+    rm -rf ${RELEASE_DIR}
 fi
+mkdir -p ${RELEASE_DIR}
 
 # 1. 先编译easy库文件
 # mkdir的-p选项允许一次创建多层次的目录，而不是一次只创建单独的目录
-if [ -e ${BUILD_ROOT}/build ]; then
-    echo "rm ${BUILD_ROOT}/build"
-    rm -rf ${BUILD_ROOT}/build
+# 设置build编译目录
+BUILD_DIR=${BUILD_ROOT}/build
+if [ -e ${BUILD_DIR} ]; then
+    echo "rm -rf ${BUILD_DIR}"
+    rm -rf ${BUILD_DIR}
 fi
-mkdir -p $BUILD_ROOT/build
-cd $BUILD_ROOT/build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
 
 # 要编译库的CMakeLists.txt目录
 EASY_ROOT=$PROJ_ROOT/src
-
-cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX:PATH=$BUILD_ROOT/dist $EASY_ROOT
+#cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX:PATH=${RELEASE_DIR} $EASY_ROOT
 make clean
 make -j4
 make install
 cd -
 
 # 2. 再编译执行文件
-if [ -e $BUILD_ROOT/test_sdk/build ]; then
-    echo "rm $BUILD_ROOT/test_sdk/build"
-    rm -rf $BUILD_ROOT/test_sdk/build
+TEST_BUILD_DIR=$BUILD_ROOT/test_sdk/build
+if [ -e $TEST_BUILD_DIR ]; then
+    echo "rm $TEST_BUILD_DIR"
+    rm -rf $TEST_BUILD_DIR
 fi
-mkdir -p $BUILD_ROOT/test_sdk/build
-cd $BUILD_ROOT/test_sdk/build
+mkdir -p $TEST_BUILD_DIR
+cd $TEST_BUILD_DIR
 
 cmake -DCMAKE_BUILD_TYPE=Release  \
--DCMAKE_INSTALL_PREFIX:PATH=$PROJ_ROOT/dist $PROJ_ROOT
+-DCMAKE_INSTALL_PREFIX:PATH=$RELEASE_DIR $PROJ_ROOT
 make clean
 make -j4
 make install
@@ -46,6 +52,6 @@ cd -
 
 # 3. 执行,先设置库文件路径
 # export LD_LIBRARY_PATH={PROJ_ROOT}/install/lib/math:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${PROJ_ROOT}/dist/lib
+export LD_LIBRARY_PATH=${RELEASE_DIR}/lib
 echo "LD_LIBRARY_PATH:${LD_LIBRARY_PATH}"
-./dist/bin/Sample
+${RELEASE_DIR}/bin/Sample
