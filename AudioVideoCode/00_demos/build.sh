589bef612559cb06785b154256e0b05ae3bf0ab0
Youngfellow <Youngfellow@163.com>
2023-11-22 14:50:44 +0800

    xxxx

--- AudioVideoCode/00_demos/build.sh
+++ AudioVideoCode/00_demos/build.sh
@@ -4,13 +4,13 @@ PROJ_ROOT=$PWD
 BUILD_ROOT=${PROJ_ROOT}
 echo "build root: ${BUILD_ROOT}"
 
-# 设置install目录
-INSTALL_DIR=${BUILD_ROOT}/install
-if [ -e ${INSTALL_DIR} ]; then
-    echo "rm -rf ${INSTALL_DIR}"
-    rm -rf ${INSTALL_DIR}
+# 设置Release目录
+RELEASE_DIR=${BUILD_ROOT}/Release
+if [ -e ${RELEASE_DIR} ]; then
+    echo "rm -rf ${RELEASE_DIR}"
+    rm -rf ${RELEASE_DIR}
 fi
-mkdir -p ${INSTALL_DIR}
+mkdir -p ${INSTALLRELEASE_DIR_DIR}
 
 # 设置build编译目录
 BUILD_DIR=${BUILD_ROOT}/build
@@ -23,7 +23,7 @@ cd ${BUILD_DIR}
 
 # 编译
 cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX:PATH=${PROJ_ROOT}/install $PROJ_ROOT
+-DCMAKE_INSTALL_PREFIX:PATH=${RELEASE_DIR} $PROJ_ROOT
 make -j4
 make install
 cd -
@@ -35,7 +35,7 @@ cd -
 # ${BUILD_ROOT}/install/bin/Demozlib
 
 # 遍历指定目录下所有文件，包括子目录下的所有文件,并运行
-BIN_DIR=${PROJ_ROOT}/install/bin
+BIN_DIR=${RELEASE_DIR}/bin
 for name in $(find $BIN_DIR -type f -name "*"); do
     echo -e "\nrun::${name}"
     ${name}
