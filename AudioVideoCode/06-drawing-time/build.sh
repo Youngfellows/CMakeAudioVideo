107e17c2a5219d089d98aec9518e14baf2f22ccd
Youngfellow <Youngfellow@163.com>
2023-11-22 16:33:12 +0800

    xxxx

--- AudioVideoCode/06-drawing-time/build.sh
+++ AudioVideoCode/06-drawing-time/build.sh
@@ -12,13 +12,13 @@ if [ -e ${RESOURCE_DIR} ]; then
 fi
 mkdir -p ${RESOURCE_DIR}
 
-# 设置install目录
-INSTALL_DIR=${BUILD_ROOT}/install
-if [ -e ${INSTALL_DIR} ]; then
-    echo "rm -rf ${INSTALL_DIR}"
-    rm -rf ${INSTALL_DIR}
+# 设置Release目录
+RELEASE_DIR=${BUILD_ROOT}/Release
+if [ -e ${RELEASE_DIR} ]; then
+    echo "rm -rf ${RELEASE_DIR}"
+    rm -rf ${RELEASE_DIR}
 fi
-mkdir -p ${INSTALL_DIR}
+mkdir -p ${RELEASE_DIR}
 
 # 设置build编译目录
 BUILD_DIR=${BUILD_ROOT}/build
@@ -30,18 +30,19 @@ mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 # 编译
-cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX:PATH=${PROJ_ROOT}/install $PROJ_ROOT
+#cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX:PATH=${RELEASE_DIR} $PROJ_ROOT
 make -j4
 make install
 cd -
 
 # 运行
-${BUILD_ROOT}/install/bin/06-drawing-time-rainbow
-${BUILD_ROOT}/install/bin/06-drawing-time-checkerboard
-${BUILD_ROOT}/install/bin/06-drawing-time-gradient
-${BUILD_ROOT}/install/bin/06-drawing-time-mandelbrot-set
-${BUILD_ROOT}/install/bin/06-drawing-time-julia-set
-${BUILD_ROOT}/install/bin/06-drawing-time-magnifying-glass
-${BUILD_ROOT}/install/bin/06-drawing-time-swirl-effect
-${BUILD_ROOT}/install/bin/06-drawing-time-frosted-glass-effect
+${RELEASE_DIR}/bin/06-drawing-time-rainbow
+${RELEASE_DIR}/bin/06-drawing-time-checkerboard
+${RELEASE_DIR}/bin/06-drawing-time-gradient
+${RELEASE_DIR}/bin/06-drawing-time-mandelbrot-set
+${RELEASE_DIR}/bin/06-drawing-time-julia-set
+${RELEASE_DIR}/bin/06-drawing-time-magnifying-glass
+${RELEASE_DIR}/bin/06-drawing-time-swirl-effect
+${RELEASE_DIR}/bin/06-drawing-time-frosted-glass-effect
